5c729726689a4a89d2081eb824892e3790fd883f
Samuel Candela <candela.samuel40@gmail.com>
2021-02-05 17:16:15 +0100

    Toolbar-v0.1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-app-bar dark color="red">
+      <v-toolbar-title @click="goHome" class="white--text">{{ $t("NavBar.title1") }}</v-toolbar-title>
+      <v-toolbar-title @click="goHeroes" class="white--text">{{ $t("NavBar.title2") }}</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data: function () {
+    return {
+      drawer: false
+    }
+  },
+  methods: {
+    goHome () {
+      this.$router.replace('/')
+    },
+    goHeroes () {
+        this.$router.replace('/Heroes')
+    }
+  }
+}
+</script>
+
