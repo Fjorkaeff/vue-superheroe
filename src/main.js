6bcb6520857198d706069a1011eef008e5e084e4
Samuel Candela <candela.samuel40@gmail.com>
2021-02-10 17:35:46 +0100

    Loading component

--- src/main.js
+++ src/main.js
@@ -6,23 +6,13 @@ import router from './router'
 import i18n from './i18n'
 import FlagIcon from 'vue-flag-icon'
 import store from './store/store.js'
-/*import Axios from 'axios';
-const CryptoJS = require("crypto-js");
-
-const PRIV_KEY = "2b101cf909b39cb27b679ea471287e2e2ba2aa81";
-const PUB_KEY = "e23507931830c9ee423da4a822ea0574";
-
-const ts = Date.now();
-const hash = CryptoJS.MD5(ts + PRIV_KEY + PUB_KEY).toString();
-const url = "http://gateway.marvel.com/v1/public/characters";*/
+import loader from 'vue-ui-preloader';
 
 Vue.use(FlagIcon);
+Vue.use(loader);
 Vue.config.productionTip = false
 
 new Vue({
-  beforeCreate: function() {
-    //this.$store.dispatch('getHeroesListFromMarvel')
-  },
   vuetify,
   router,
   i18n,
--- src/views/Heroes.vue
+++ src/views/Heroes.vue
@@ -72,7 +72,7 @@ export default {
                     </v-row>
                 </div>
                 <div v-if="isLoading">
-                    Loading <v-icon>fas fa-circle-notch fa-spin</v-icon>
+                    <loader object="#ff9633" color1="#ffffff" color2="#17fd3d" size="5" speed="2" bg="#343a40" objectbg="#999793" opacity="80" name="dots"></loader>
                 </div>
                 <div v-if="this.DisplayList && !isLoading">
                     <HeroDisplayRow v-for="hero in heroes" :key="hero.id" :hero="hero"></HeroDisplayRow>
